a22bdb67a088eda223a65a69974ac372bc9b0fe1
afchine said <afzongho.code@gmail.com>
2021-06-30 16:53:53 +0200

    addition of remove button on playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -24,6 +24,7 @@ class App extends React.Component {
 
 
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   };
 
 
@@ -34,7 +35,14 @@ addTrack(track) {
   }
 
   tracks.push(track);
-  this.setState({ playlistTracks: tracks })
+  this.setState({ playlistTracks: tracks });
+}
+
+removeTrack(track) {
+  let tracks = this.state.playlistTracks;
+  tracks= tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+  this.setState({playlistTracks: tracks});
 }
 
 
@@ -48,7 +56,8 @@ addTrack(track) {
     <SearchResults searchResults={this.state.searchResults}
                   onAdd={this.addTrack} />
     <Playlist playlistName={this.state.playlistName}
-              playlistTracks={this.state.playlistTracks} />
+              playlistTracks={this.state.playlistTracks} 
+              onRemove= {this.removeTrack}/>
     </div>
   </div>
 </div>
